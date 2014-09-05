9b20f252da82a02a6201f6ccdc8bc8ee670fe3b3
Jamodon <jamodonnell@gmail.com>
2014-09-05 09:00:22 -0400

    lunch 2

---
+++ lunch2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#Day 2 - Lunch
+
+f = open(accepted_hits.sam)
+
+nl = 0
+while True:
+    line = f.readline()
+    if line == ""
+        break
+    elif (^SRR*) in line:
+        nl = nl + 1
+print nl
